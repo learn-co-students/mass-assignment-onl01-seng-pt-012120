abbfea3b4a51b9b10f34063e1b9c665ff46bcdb3
Prince Charles <princecharles35@yahoo.com>
2020-01-22 16:37:33 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,12 @@
-class Person
-  #your code here
-end
+bob_attributes = {name: "Bob", hair_color: "Brown"}
+ 
+bob = Person.new(bob_attributes)
+bob.name       # => "Bob"
+bob.hair_color # => "Brown"
+ 
+susan_attributes = {name: "Susan", height: "5'11\"", eye_color: "Green"}
+ 
+susan = Person.new(susan_attributes)
+susan.name      # => "Susan"
+susan.height    # => "5'11""
+susan.eye_color # => "Green"
